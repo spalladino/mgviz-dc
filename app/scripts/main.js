0f55abfa82f703a2b908af512b89908f8ea3c89c
Santiago Palladino <spalladino@gmail.com>
2015-07-08 19:09:13 -0300

    Minor refactor for time series

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -49,42 +49,28 @@ var syndromesCountChart = dc.dataCount('#syndromesCount').dimension(syndromesDat
 
 // Time series chart
 
-var timeChart = dc.lineChart("#timeSeries");
+var buildTimeChart = function(target, dataset) {
+  var volumeByHour = dataset.dimension(function(d) {
+    return d3.time.day(new Date(d.date_onset));
+  });
 
-//worldcup_data.forEach(function(d){
-//  d["Date of Report"] = new Date(Date.parse(d["Date of Report"]));
-//  //d3.time.format("%m/%d/%Y %I:%M").parse(d["Date of Report"]);
-//  //console.log(d["Date of Report"])
-//  symptoms = ["Fever","Cough","Sore Throat","Shortness of Breath","Nausea,Diarrhea",
-//    "Joint Pain","Headache","Bleeding","Rash"];
-//  d["symptoms"] = [];
-//  for (val in symptoms){
-//    if(d[symptoms[val]] == "1"){
-//      d["symptoms"].push(symptoms[val]);
-//      // console.log(d["symptoms"]);
-//    }
-//  }
-//
-//});
+  var volumeByHourGroup = volumeByHour.group().reduceCount(function(d) {
+    return d.date_onset;
+  });
 
-var volumeByHour = syndromesDataset.dimension(function(d) {
-  return d3.time.day(new Date(d.date_onset));
-});
+  return dc.lineChart(target).width(1000)
+    .height(200)
+    .margins({top: 10, right: 10, bottom: 20, left: 40})
+    .dimension(volumeByHour)
+    .group(volumeByHourGroup)
+    .transitionDuration(500)
+    .elasticY(true)
+    .x(d3.time.scale()) //x(d3.time.scale().domain([new Date(2014, 1, 1), new Date(2014, 12, 31)]))
+    .elasticX(true)
+    .xAxis();
+};
 
-var volumeByHourGroup = volumeByHour.group().reduceCount(function(d) {
-  return d.date_onset;
-});
-
-timeChart.width(1000)
-  .height(200)
-  .margins({top: 10, right: 10, bottom: 20, left: 40})
-  .dimension(volumeByHour)
-  .group(volumeByHourGroup)
-  .transitionDuration(500)
-  .elasticY(true)
-  .x(d3.time.scale()) //x(d3.time.scale().domain([new Date(2014, 1, 1), new Date(2014, 12, 31)]))
-  .elasticX(true)
-  .xAxis();
+var syndromesTimeSeries = buildTimeChart(timeSeries, syndromesDataset);
 
 
 // Go!

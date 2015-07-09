3017668895dbbefba31a1fe465e0ff31b6a833dc
Eyvind Niklasson <eyvind.niklasson@gmail.com>
2015-07-09 12:05:38 -0300

    More fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,6 +23,23 @@ var setFilters = function(filters) {
 };
 var userAlerts = [{"alert_date":"2015-06-07","alert_syndrome":"Diarreica","alert_region":"Brasilia"}];
 
+// Alerts management
+
+var setFilters = function(filters) {
+syndromesChart.filter(filters.syndrome);
+dc.redrawAll();
+//  if (filters.syndrome) {
+//    syndromesChart.filter(filters.syndrome);
+//    dc.redrawAll();
+//    $('#syndromesTab').tab('show');
+//  } else if (filters.symptom) {
+//    symptomsChart.filter(filters.symptom);
+//    dc.redrawAll();
+//    $('#symptomsTab').tab('show');
+//  }
+};
+
 // Go!
 
 dc.renderAll();
+

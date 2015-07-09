b7ecf986ef0511f007d5a3298a669417931bcb0e
Eyvind Niklasson <eyvind.niklasson@gmail.com>
2015-07-09 02:55:04 -0300

    Implemented stacking of symptoms in the timeseries. Also did a lot of d3 deep diving and hacking to make them clickable, as this is not a feature in dc.js, so i re-implemented it myself by looking for the path area elements!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -38,105 +38,100 @@ var countChart = dc.dataCount('#dataCount').dimension(symptomsDataset).group(all
 // Time series chart
 
 var timeChart = dc.lineChart("#timeSeries");
-
-//worldcup_data.forEach(function(d){
-//  d["Date of Report"] = new Date(Date.parse(d["Date of Report"]));
-//  //d3.time.format("%m/%d/%Y %I:%M").parse(d["Date of Report"]);
-//  //console.log(d["Date of Report"])
-//  symptoms = ["Fever","Cough","Sore Throat","Shortness of Breath","Nausea,Diarrhea",
-//    "Joint Pain","Headache","Bleeding","Rash"];
-//  d["symptoms"] = [];
-//  for (val in symptoms){
-//    if(d[symptoms[val]] == "1"){
-//      d["symptoms"].push(symptoms[val]);
-//      // console.log(d["symptoms"]);
-//    }
-//  }
-//
-//});
+var navChart = dc.barChart("#timeNavigation");
 
 var volumeByHour = symptomsDataset.dimension(function(d) {
-  return d3.time.day(new Date(d.date_onset));
+  return new Date(d.date_onset);
 });
 
-var volumeByHourGroup = volumeByHour.group().reduceCount(function(d) {
-  return d.date_onset;
-});
+var volumeByHourGroup = volumeByHour.group(
+  function(the_date){
+    return d3.time.day(the_date); // TODO make the granularity easier to see 
+  }
+);
 
-timeChart.width(1000)
+//For some very strange reason reduceCount is not working - it is simply never
+//called when group().reduceCount() is run on a set - 
+// - could this be because we are using a bleeding edge version of dc.js?
+//So instead we just do some hacking - using reduceSum as a reduceCount alternative
+
+var symptomGroupsTimeSeries = 
+  volumeByHour
+  .group(function(date){
+      return d3.time.day(date); // TODO make the granularity easier to see 
+  })
+  .reduce(
+    function(p, d){
+      p[d.symptom] = (p[d.symptom] || 0) + 1;
+      return p;  
+    },
+    function(p, d) {
+      --p[d.symptom];
+      return p;
+    },
+    function() { return {}; }
+  );
+
+//inspired by this hack, which was actually wrongly implemented?
+//http://stackoverflow.com/questions/24415665/dc-js-stacked-area-chart-using-reducecount-method
+
+//This extracts all the symptoms we have seen!
+//We have to extract the first element here because we can't modify the array 
+// as everything here is lazy and computed asynchronously!
+console.log(symptomsGroup.top(Infinity));
+var observed_symptoms = symptomsGroup.top(Infinity).map(function(obj){ return obj.key; });
+var first_symptom = observed_symptoms[0];
+observed_symptoms.shift();
+
+timeChart
+  .width(800)
   .height(200)
+  .renderArea(true)
   .margins({top: 10, right: 10, bottom: 20, left: 40})
   .dimension(volumeByHour)
-  .group(volumeByHourGroup)
+  .group(symptomGroupsTimeSeries, first_symptom, function(d){
+    return d.value[first_symptom] || 0;
+  })
+  .brushOn(false) // we need this for the clickable hack haha
+
+observed_symptoms.forEach(function(field, i){
+  timeChart.stack(symptomGroupsTimeSeries, observed_symptoms[i], function(d){
+    return d.value[field] || 0;
+  });
+});
+
+timeChart
+  .rangeChart(navChart)
   .transitionDuration(500)
   .elasticY(true)
-  .x(d3.time.scale()) //x(d3.time.scale().domain([new Date(2014, 1, 1), new Date(2014, 12, 31)]))
-  .elasticX(true)
-  .xAxis();
+  .x(d3.time.scale().domain([new Date(2015, 5, 1), new Date(2015, 6, 1)]))
+  .xUnits(d3.time.days)
+  .xAxis()
 
-// var symptomsChart = dc.rowChart('#symptomsChart');
+//This hack is terrible but dives into d3 to find the area entities in the 
+//svg and makes them clickable.
+timeChart.on('renderlet.timechart', function(chart, filter) {
+    chart.selectAll("path.area").on("click.timechart", (
+      function(d) {
+        symptomsChart.filter([d.name]);
+        dc.redrawAll();
+      }
+    ));
+  });
 
-// var reduceAdd = function(p, v) {
-//   v.symptoms.forEach (function(val, idx) {
-//     p[val] = (p[val] || 0) + 1;
-//   });
-//   return p;
-// }
+//onclick issue maybe fixed here https://github.com/dc-js/dc.js/issues/168
 
-// var reduceRemove = function(p, v) {
-//   v.symptoms.forEach (function(val, idx) {
-//     p[val] = (p[val] || 0) - 1;
-//   });
-//   return p;
-
-// }
-
-// var reduceInitial = function() {
-//   return {};
-// }
-
-// var symptoms = dataset.dimension(function (d) {
-//   return d.symptoms; });
-// var symptomsGroup = symptoms.groupAll().reduce(reduceAdd, reduceRemove, reduceInitial).value();
-
-// // Hack from http://stackoverflow.com/a/17529113/12791
-// symptomsGroup.all = function() {
-//   var newObject = [];
-//   for (var key in this) {
-//     if (this.hasOwnProperty(key) && key != "all") {
-//       newObject.push({
-//         key: key,
-//         value: this[key]
-//       });
-//     }
-//   }
-//   return newObject;
-// }
-
-// symptomsChart.width(1000)
-//   .height(200)
-//   .margins({top: 20, left: 10, right: 10, bottom: 20})
-//   .group(symptomsGroup)
-//   .dimension(symptoms)
-//   .filterHandler(function(dimension, filter){
-//     dimension.filter(function(d) { return filter != null ? _.some(d, function(s) { return _.contains(filter, s) }) : true;});
-//     return filter;
-//   })
-//   .elasticX(true)
-//   .label(function (d) {
-//     return d.key;
-//   })
-//   .title(function (d) {
-//     return d.value;
-//   });
-
-
-
-// Data count widget
-
-//var countChart = dc.dataCount('#dataCount').dimension(crossfiltered).groupAll();
-
-
-// Go!
+navChart.width(800)
+  .height(40)
+  .margins({top: 0, right: 50, bottom: 20, left: 40})
+  .dimension(volumeByHour)
+  .group(volumeByHourGroup)
+  .centerBar(true)
+  .gap(1)
+  //.elasticX(true)
+  .x(d3.time.scale().domain([new Date(2015, 5, 1), new Date(2015, 6, 1)]))
+  .round(d3.time.days.round)
+  .alwaysUseRounding(true)
+  .xUnits(d3.time.days);
 
 dc.renderAll();

6008f5a022c9a84045206b7913b3554cef48116f
Greg Denehy <greg@inclose.com.au>
2015-07-09 12:31:45 -0300

    Implemented cvs export

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -55,3 +55,48 @@ var symptomsGroup = symptomsDimension.group();
 
 var syndromesDimension = syndromesDataset.dimension(function (d) { return d.syndrome; });
 var syndromesGroup = syndromesDimension.group();
+
+
+function exportSymptoms() {
+  exportData(symptomsDimension.top(Infinity), "symptoms");
+}
+
+function exportSyndromes() {
+  exportData(syndromesDimension.top(Infinity), "syndromes");
+}
+
+function exportData(data, type) {
+  if (data.length > 0) {
+    // prepare CSV data
+    var csvData = new Array();
+    var header = [];
+    for(var prop in data[0]) {
+      header.push('"' + prop + '"');
+    }
+    csvData.push(header.join(','));
+    data.forEach(function(item, index, array) {
+      var row = [];
+      for(var prop in item) {
+        row.push('"' + item[prop] + '"');
+      }
+      csvData.push(row.join(','));
+    });
+
+    // download stuff
+    var fileName = "data-" + type + ".csv";
+    var buffer = csvData.join("\n");
+    var blob = new Blob([buffer], {
+      "type": "text/csv;charset=utf8;"			
+    });
+    var link = document.createElement("a");
+    if(link.download !== undefined) { // feature detection
+      // Browsers that support HTML5 download attribute
+      link.setAttribute("href", window.URL.createObjectURL(blob));
+      link.setAttribute("download", fileName);
+      link.click();
+     }
+    else {
+      // it needs to implement server side export
+    }
+  }
+}

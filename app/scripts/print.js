05459730de61f7aef307fe7ab9064da4da5ec8a9
Greg Denehy <greg@inclose.com.au>
2015-07-09 10:47:00 -0300

    Support for no-print class

--- app/scripts/print.js
+++ app/scripts/print.js
@@ -1,3 +1,3 @@
 $(".nav-tabs li a").each( function() {       
-    $("#" + $(this).attr('data-toggle')).prepend("<h2 class='print'>" + $(this).text() + "</h2>");
+    $("#" + $(this).attr('data-toggle')).prepend("<h2 class='print-only'>" + $(this).text() + "</h2>");
 });

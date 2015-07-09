7a38279e68c998fb354297921e93a70be9d59f8d
Eyvind Niklasson <eyvind.niklasson@gmail.com>
2015-07-09 11:47:23 -0300

    Made the line graph multiline again! Colors are predefined at the moment, ideas?

--- app/scripts/charts/timeseries.js
+++ app/scripts/charts/timeseries.js
@@ -2,6 +2,95 @@
 
 var buildTimeChart = function(dataset, group, accessor, target, navigation) {
 
+  var symptomsTimeChart = dc.compositeChart(target);
+  var symptomsNavChart = dc.barChart(navigation);
+
+  var volumeByHour = dataset.dimension(function(d) {
+    return new Date(d.date_onset);
+  });
+
+  var volumeByHourGroup = volumeByHour.group(
+    function(the_date){
+      return d3.time.day(the_date); // TODO make the granularity easier to see
+    }
+  );
+
+  var symptomGroupsTimeSeries =
+    volumeByHour
+    .group(function(date){
+        return d3.time.day(date); // TODO make the granularity easier to see
+    })
+    .reduce(
+      function(p, d){
+        p[d[accessor]] = (p[d[accessor]] || 0) + 1;
+        return p;
+      },
+      function(p, d) {
+        --p[d[accessor]];
+        return p;
+      },
+      function() { return {}; }
+    );
+
+  var observed_symptoms = group.top(Infinity).map(function(obj){ return obj.key; });
+
+  symptomsTimeChart
+    .width(1100)
+    .height(300)
+    .margins({top: 10, right: 10, bottom: 20, left: 40})
+    .rangeChart(symptomsNavChart)
+    .transitionDuration(100)
+    .elasticY(true)
+    .x(d3.time.scale().domain([new Date(2015, 5, 1), new Date(2015, 5, 19)]))
+    .xUnits(d3.time.days)
+    .xAxis()
+
+  var theLines = [];
+  var colorsSymptoms = ["red", "green", "blue", "yellow", "black", "orange", "purple"]; 
+  //for now we just give a few colors to choose from? who knows what to do?
+  //maybe some hash function on the name of the symptom that maps name -> color
+
+  observed_symptoms.forEach(function(field, i){
+    theLines.push(
+      dc.lineChart(symptomsTimeChart)
+        .dimension(volumeByHour)
+        .colors(colorsSymptoms[i])
+        .group(symptomGroupsTimeSeries, observed_symptoms[i], function(d){
+          return d.value[field] || null;  
+        })
+    );
+  });
+
+  symptomsTimeChart.compose(theLines);
+
+  symptomsTimeChart
+    .rangeChart(symptomsNavChart)
+    .transitionDuration(100)
+    .elasticY(true)
+    .x(d3.time.scale().domain([new Date(2015, 5, 1), new Date(2015, 5, 19)]))
+    .xUnits(d3.time.days)
+    .xAxis()
+
+  return symptomsNavChart.width(1140)
+    .height(60)
+    .margins({top: 0, right: 50, bottom: 20, left: 40})
+    .dimension(volumeByHour)
+    .group(volumeByHourGroup)
+    .centerBar(true)
+    .gap(1)
+    //.elasticX(true)
+    .x(d3.time.scale().domain([new Date(2015, 5, 1), new Date(2015, 5, 19)]))
+    .round(d3.time.days.round)
+    .alwaysUseRounding(true)
+    .xUnits(d3.time.days)
+    .yAxis().ticks(0);
+
+
+/*
+
+OLD STACKED CHARTS, DO NOT REMOVE AT THE MOMEN
+
+
   var symptomsTimeChart = dc.lineChart(target);
   var symptomsNavChart = dc.barChart(navigation);
 
@@ -103,7 +192,7 @@ var buildTimeChart = function(dataset, group, accessor, target, navigation) {
     .alwaysUseRounding(true)
     .xUnits(d3.time.days)
     .yAxis().ticks(0);
-
+*/
 };
 
 buildTimeChart(syndromesDataset, syndromesGroup, 'syndrome', '#syndromesTimeSeries', '#syndromesTimeNavigation');
